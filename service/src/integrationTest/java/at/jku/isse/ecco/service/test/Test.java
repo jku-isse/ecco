bbb93d369553d35729de6224a9cddaa491318e22
llinsbauer <llinsbauer@users.noreply.github.com>
2018-05-08 18:22:05 +0200

    file locking in ser transaction strategy

--- service/src/integrationTest/java/at/jku/isse/ecco/service/test/Test.java
+++ service/src/integrationTest/java/at/jku/isse/ecco/service/test/Test.java
@@ -2,17 +2,78 @@ package at.jku.isse.ecco.service.test;
 
 import at.jku.isse.ecco.EccoException;
 import at.jku.isse.ecco.EccoService;
+import at.jku.isse.ecco.storage.ser.dao.SerTransactionStrategy;
 import at.jku.isse.ecco.tree.RootNode;
 
 import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class Test {
+
+
+	@org.testng.annotations.Test(groups = {"integration", "base", "service"})
+	public void FileLockTest2() throws IOException {
+		SerTransactionStrategy ts = new SerTransactionStrategy(Paths.get("C:\\Users\\user\\Desktop\\ECCO_TEST\\repo"));
+
+		ts.open();
+
+		ts.begin();
+
+		ts.end();
+
+		ts.close();
+	}
+
+	@org.testng.annotations.Test(groups = {"integration", "base", "service"})
+	public void FileLockTest() throws IOException {
+		Path idFile = Paths.get("C:/Users/user/Desktop/ECCO_TEST/idfile");
+		Path writeLockFile = Paths.get("C:/Users/user/Desktop/ECCO_TEST/writelockfile");
+
+		// check if write lock file exists and if not create it
+		if (!Files.exists(writeLockFile)) {
+			Files.createFile(writeLockFile);
+		}
+
+		// obtain exclusive write lock
+		{
+			FileChannel writeFileChannel = FileChannel.open(writeLockFile, StandardOpenOption.WRITE);
+			FileLock writeFileLock = writeFileChannel.lock(0, Long.MAX_VALUE, false);
+			if (!writeFileLock.isValid())
+				throw new EccoException("Could not obtain exclusive lock on WRITE file.");
+			System.out.println("AAA: " + writeFileLock.isShared());
+			writeFileChannel.lock(0, Long.MAX_VALUE, true);
+			System.out.println("AAA: " + writeFileLock.isShared());
+		}
+		{
+			FileChannel writeFileChannel = FileChannel.open(writeLockFile, StandardOpenOption.WRITE);
+			FileLock writeFileLock = writeFileChannel.lock(0, Long.MAX_VALUE, false);
+			if (!writeFileLock.isValid())
+				throw new EccoException("Could not obtain exclusive lock on WRITE file.");
+			System.out.println("AAA: " + writeFileLock.isShared());
+		}
+
+		// check if id file exists and if not create it and write new id
+		if (!Files.exists(idFile)) {
+			Files.write(idFile, UUID.randomUUID().toString().getBytes(Charset.defaultCharset()), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
+		}
+
+		// get shared lock on id file, read id, set it as current, release lock
+		try (FileChannel idFileChannel = FileChannel.open(idFile, StandardOpenOption.READ); FileLock idFileLock = idFileChannel.lock(0, Long.MAX_VALUE, true)) {
+			if (!idFileLock.isValid())
+				throw new EccoException("Could not obtain shared lock on ID file.");
+		}
+	}
+
 
 	@org.testng.annotations.Test(groups = {"integration", "base", "service"})
 	public void Test() throws EccoException {
--- service/src/main/java/at/jku/isse/ecco/storage/ser/dao/SerTransactionStrategy.java
+++ service/src/main/java/at/jku/isse/ecco/storage/ser/dao/SerTransactionStrategy.java
@@ -10,6 +10,7 @@ import com.google.inject.name.Named;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.nio.charset.Charset;
@@ -49,6 +50,9 @@ public class SerTransactionStrategy implements TransactionStrategy {
 	// currently loaded database object
 	protected Database database;
 
+	protected FileLock writeFileLock;
+	protected FileChannel writeFileChannel;
+
 	protected TRANSACTION transaction;
 
 	protected int transactionCounter;
@@ -75,6 +79,7 @@ public class SerTransactionStrategy implements TransactionStrategy {
 		this.database = null;
 		this.transaction = null;
 		this.transactionCounter = 0;
+		this.writeFileLock = null;
 	}
 
 
@@ -137,8 +142,17 @@ public class SerTransactionStrategy implements TransactionStrategy {
 
 
 	protected void beginReadOnly() throws IOException, ClassNotFoundException {
+		if (this.transaction == TRANSACTION.READ_ONLY) // nothing to do, we already have a read transaction going
+			return;
+
 		if (this.transaction == null)
 			this.transaction = TRANSACTION.READ_ONLY;
+
+		// check if id file exists and if not the repository is empty
+		if (!Files.exists(this.idFile)) {
+			this.database = new Database();
+			return;
+		}
 
 		// get shared lock on id file, read id, set it as current, release lock
 		try (FileChannel fileChannel = FileChannel.open(this.idFile, StandardOpenOption.READ); FileLock fileLock = fileChannel.lock(0, Long.MAX_VALUE, true)) {
@@ -158,7 +172,7 @@ public class SerTransactionStrategy implements TransactionStrategy {
 		if (Files.exists(this.dbFile)) {
 			this.database = (Database) deserialize(this.dbFile);
 		} else {
-			throw new EccoException("DB file does not exist.");
+			throw new EccoException("DB file does not exist: " + this.dbFile);
 		}
 	}
 
@@ -167,15 +181,21 @@ public class SerTransactionStrategy implements TransactionStrategy {
 
 
 	protected void beginReadWrite() throws IOException, ClassNotFoundException {
+		if (this.transaction == TRANSACTION.READ_ONLY) // the transaction started as a read only transaction and cannot be elevated to a read write transaction
+			throw new EccoException("Cannot elevate a read only transaction to a read write transaction.");
+
+		if (this.transaction == TRANSACTION.READ_WRITE)
+			return; // nothing to do, we already have a read/write transaction going
+
 		// check if write lock file exists and if not create it
 		if (!Files.exists(this.writeLockFile)) {
 			Files.createFile(this.writeLockFile);
 		}
 
 		// obtain exclusive write lock
-		FileChannel writeFileChannel = FileChannel.open(this.writeLockFile, StandardOpenOption.WRITE);
-		FileLock writeFileLock = writeFileChannel.lock(0, Long.MAX_VALUE, false);
-		if (!writeFileLock.isValid())
+		this.writeFileChannel = FileChannel.open(this.writeLockFile, StandardOpenOption.WRITE);
+		this.writeFileLock = writeFileChannel.lock(0, Long.MAX_VALUE, false);
+		if (!this.writeFileLock.isValid())
 			throw new EccoException("Could not obtain exclusive lock on WRITE file.");
 
 		this.transaction = TRANSACTION.READ_WRITE;
@@ -199,9 +219,10 @@ public class SerTransactionStrategy implements TransactionStrategy {
 				return;
 			this.id = id;
 		}
-		this.dbFile = this.repositoryDir.resolve(this.id + DB_FILE_SUFFIX);
 
-		if (Files.exists(this.dbFile)) {
+		Path dbFile = this.repositoryDir.resolve(this.id + DB_FILE_SUFFIX);
+		if (Files.exists(dbFile)) {
+			this.dbFile = dbFile;
 			try (FileChannel dbFileChannel = FileChannel.open(this.idFile, StandardOpenOption.READ); FileLock dbFileLock = dbFileChannel.lock(0, Long.MAX_VALUE, true)) {
 				if (!dbFileLock.isValid())
 					throw new EccoException("Could not obtain shared lock on DB file.");
@@ -216,39 +237,42 @@ public class SerTransactionStrategy implements TransactionStrategy {
 
 	protected void endReadWrite() throws IOException {
 		// check if we still have exclusive write lock and take it
-		try (FileChannel writeFileChannel = FileChannel.open(this.writeLockFile, StandardOpenOption.WRITE); FileLock writeFileLock = writeFileChannel.lock(0, Long.MAX_VALUE, false)) {
-			if (!writeFileLock.isValid())
-				throw new EccoException("Could not obtain exclusive lock on WRITE file.");
+		if (!this.writeFileLock.isValid())
+			throw new EccoException("Lost exclusive lock on WRITE file.");
 
-			// compute new random id
-			String newId = UUID.randomUUID().toString();
-			// serialize to new db file
-			Path newDbFile = this.repositoryDir.resolve(newId + DB_FILE_SUFFIX);
-			serialize(this.database, newDbFile);
+		// compute new random id
+		String newId = UUID.randomUUID().toString();
+		// serialize to new db file
+		Path newDbFile = this.repositoryDir.resolve(newId + DB_FILE_SUFFIX);
+		serialize(this.database, newDbFile);
 
-			// obtain exclusive lock on id file, write new id, update current id and db file, release lock
-			try (FileChannel idFileChannel = FileChannel.open(this.idFile, StandardOpenOption.WRITE); FileLock idFileLock = idFileChannel.lock(0, Long.MAX_VALUE, false)) {
-				if (!idFileLock.isValid())
-					throw new EccoException("Could not obtain exclusive lock on ID file.");
+		// obtain exclusive lock on id file, write new id, update current id and db file, release lock
+		try (FileChannel idFileChannel = FileChannel.open(this.idFile, StandardOpenOption.WRITE); FileLock idFileLock = idFileChannel.lock(0, Long.MAX_VALUE, false)) {
+			if (!idFileLock.isValid())
+				throw new EccoException("Could not obtain exclusive lock on ID file.");
 
-				// write new id to id file
-				Files.write(this.idFile, newId.getBytes(Charset.defaultCharset()), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
+			// write new id to id file
+			idFileChannel.write(ByteBuffer.wrap(newId.getBytes(Charset.defaultCharset())));
+			//Files.write(this.idFile, newId.getBytes(Charset.defaultCharset()), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
 
-				// delete old db file if nobody has a shared lock anymore (i.e. if we can get an exclusive lock on it)
+			// delete old db file if nobody has a shared lock anymore (i.e. if we can get an exclusive lock on it)
+			if (this.dbFile != null) {
 				try (FileChannel oldDbFileChannel = FileChannel.open(this.dbFile, StandardOpenOption.WRITE); FileLock oldDbFileLock = oldDbFileChannel.lock(0, Long.MAX_VALUE, false)) {
 					if (oldDbFileLock.isValid())
 						Files.delete(this.dbFile);
 				}
-
-				// update id and db file
-				this.id = newId;
-				this.dbFile = newDbFile;
-
-				// release exclusive id lock automatically when exiting try block
 			}
 
-			// release exclusive write lock automatically after try block
+			// update id and db file
+			this.id = newId;
+			this.dbFile = newDbFile;
+
+			// release exclusive id lock automatically when exiting try block
 		}
+
+		// release exclusive write lock automatically after try block
+		this.writeFileLock.close();
+		this.writeFileChannel.close();
 	}
 
 
