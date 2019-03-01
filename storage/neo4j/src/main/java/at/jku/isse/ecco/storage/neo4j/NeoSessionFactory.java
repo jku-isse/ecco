b01e00b06007f0ba8ffccb5886bc25498291d63d
chuber <kontakt@sl-huber.at>
2019-03-01 10:56:07 +0100

    Neo4J: Changed Embedded datbase creation to support Bolt

--- storage/neo4j/src/main/java/at/jku/isse/ecco/storage/neo4j/NeoSessionFactory.java
+++ storage/neo4j/src/main/java/at/jku/isse/ecco/storage/neo4j/NeoSessionFactory.java
@@ -1,7 +1,12 @@
 package at.jku.isse.ecco.storage.neo4j;
 
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
+import org.neo4j.kernel.configuration.BoltConnector;
+import org.neo4j.kernel.configuration.Connector;
 import org.neo4j.ogm.config.Configuration;
+import org.neo4j.ogm.drivers.embedded.driver.EmbeddedDriver;
 import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 
@@ -9,21 +14,34 @@ import java.nio.file.Path;
 
 public class NeoSessionFactory {
 
+    public static final String _BOLT_CONNECTION_STRING = "ecco";
     private final Path databasePath;
     private final SessionFactory factory;
 
+    // https://stackoverflow.com/questions/38077512/access-the-neo4j-browser-while-running-an-embedded-connection-with-a-bolt-connec?rq=1
     public NeoSessionFactory(Path databasePath){
         this.databasePath = databasePath;
 
-        Configuration configuration = new Configuration.Builder()
-                .uri(this.databasePath.toUri().toString())
-                .build();
+        // create embedded graph database
+        BoltConnector boltConnector = new BoltConnector(_BOLT_CONNECTION_STRING);
 
+        GraphDatabaseService graphDb = new GraphDatabaseFactory()
+                .newEmbeddedDatabaseBuilder(databasePath.toFile())
+                .setConfig(boltConnector.type, "BOLT" )
+                .setConfig(boltConnector.enabled, "true" )
+                .setConfig(boltConnector.listen_address, "localhost:7687" )
+                .setConfig(GraphDatabaseSettings.auth_enabled, "FALSE")
+                .newGraphDatabase();
+
+        registerShutdownHook(graphDb);
+
+        // connect OGM session factory to embedded database
+        EmbeddedDriver driver = new EmbeddedDriver(graphDb);
         final String[] packages = new String[] {
                 "at.jku.isse.ecco.storage.neo4j.domain",
         };
 
-        factory = new SessionFactory(configuration, packages);
+        factory = new SessionFactory(driver, packages);
     }
 
     public SessionFactory getFactory() {
@@ -32,5 +50,10 @@ public class NeoSessionFactory {
 
     public Session getNeoSession() {
         return factory.openSession();
+    }
+
+    private static void registerShutdownHook( final GraphDatabaseService graphDb )
+    {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> graphDb.shutdown()));
     }
 }
