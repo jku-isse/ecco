57e98651e7f897cb967a70719859764c2900b883
Tobias <14137285+Dorkat0@users.noreply.github.com>
2021-03-31 09:14:31 +0200

    -base repro test -set standard Adapters to dependencies

--- base/src/test/java/at/jku/isse/ecco/test/RepositoryTest.java
+++ base/src/test/java/at/jku/isse/ecco/test/RepositoryTest.java
@@ -1,5 +1,60 @@
 package at.jku.isse.ecco.test;
+import at.jku.isse.ecco.service.EccoService;
+import com.google.common.io.RecursiveDeleteOption;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import java.io.IOException;
+import static com.google.common.io.MoreFiles.deleteDirectoryContents;
 public class RepositoryTest {
+    Path basePath = Path.of("D:\\Eigene Daten\\Studium\\Studium\\LVAs\\6_Semester\\Bsc\\ecco\\examples\\image_variants"); //TODO change to relative path
 
+    @Test(groups = {"integration", "gui"})
+    public void populateSimpleVersionRepository() {
+
+        // open repository
+        EccoService service = new EccoService();
+
+        //create Repo
+        String repo = ".ecco";
+        Path p = basePath.resolve(repo);
+        try {
+            deleteDirectoryContents(p, RecursiveDeleteOption.ALLOW_INSECURE);       //TBE ALLOW INSECURE
+            Files.delete(p);        //TBE Works only if the dir is already empty. (done by  deleteDirectoryContents)
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Assert.assertFalse(Files.exists(p));
+
+        service.setRepositoryDir(p);
+        service.init();
+
+        int variantsCnt = 0;
+        service.setBaseDir(basePath.resolve("V1_purpleshirt"));
+        service.commit("V1");
+        variantsCnt++;
+
+        service.setBaseDir(basePath.resolve("V2_stripedshirt"));
+        service.commit("V2");
+        variantsCnt++;
+
+        service.setBaseDir(basePath.resolve("V3_purpleshirt_jacket"));
+        service.commit("V3");
+        variantsCnt++;
+
+        service.setBaseDir(basePath.resolve("V4_purpleshirt_jacket_glasses"));
+        service.commit("V4");
+        variantsCnt++;
+
+        System.out.printf("Committed %d variants\n", variantsCnt);
+
+
+        // close repository
+        service.close();
+        System.out.println("Repository closed.");
+    }
 }
