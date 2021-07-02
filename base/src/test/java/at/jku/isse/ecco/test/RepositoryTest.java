f15645d4640c1b097326ba981a6bad1611084b3a
Dorkat0 <14137285+Dorkat0@users.noreply.github.com>
2021-07-02 17:19:16 +0200

    added Repro. setup for a big history (Automatic Commit naming and file choosing)

--- base/src/test/java/at/jku/isse/ecco/test/RepositoryTest.java
+++ base/src/test/java/at/jku/isse/ecco/test/RepositoryTest.java
@@ -5,10 +5,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import static com.google.common.io.MoreFiles.deleteDirectoryContents;
 public class RepositoryTest {
     Path basePath = Path.of("D:\\Eigene Daten\\Studium\\Studium\\LVAs\\6_Semester\\Bsc\\ecco\\examples\\image_variants"); //TODO change to relative path
@@ -93,6 +97,37 @@ public class RepositoryTest {
         service.setBaseDir(basePath.resolve("V4_purpleshirt_jacket_glasses"));
         service.commit();
         variantsCnt++;
+    }
+
+    @Test(groups = {"integration", "gui"})
+    public void setupReproForManualInspectionBigHistory() {     //TBE
+        // open repository
+        EccoService service = new EccoService();
+
+        //create Repo
+        String repo = ".ecco";
+
+        basePath = basePath.getParent().resolve("BigHistory");
+        Path p = basePath.resolve(repo);
+        try {
+            deleteDirectoryContents(p, RecursiveDeleteOption.ALLOW_INSECURE);       //TBE ALLOW INSECURE
+            Files.delete(p);        //TBE Works only if the dir is already empty. (done by  deleteDirectoryContents)
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Assert.assertFalse(Files.exists(p));
+
+        service.setRepositoryDir(p);
+        service.init();
+
+       //int commitNumber = 1;
+        File[] files = basePath.toFile().listFiles();
+        for(int commitNumber = 1; commitNumber < files.length; commitNumber++) {
+            service.setBaseDir(basePath.resolve(files[commitNumber].getName()));
+            System.out.println(files[commitNumber].getName());
+            service.commit(commitNumber + ". Commit: " + files[commitNumber].getName());
+            //commitNumber++;
+        }
     }
 
 }
