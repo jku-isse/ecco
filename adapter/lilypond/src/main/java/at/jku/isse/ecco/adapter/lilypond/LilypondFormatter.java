0f1eb8669d36c6d1091a9c0b9f4e1ff5a9208d6c
Henry <rudi@ruka.at>
2022-08-02 19:34:50 +0200

    Refine code

--- adapter/lilypond/src/main/java/at/jku/isse/ecco/adapter/lilypond/LilypondFormatter.java
+++ adapter/lilypond/src/main/java/at/jku/isse/ecco/adapter/lilypond/LilypondFormatter.java
@@ -5,49 +5,51 @@ import at.jku.isse.ecco.adapter.lilypond.data.token.DefaultTokenArtifactData;
 class LilypondFormatter {
     @SuppressWarnings("RedundantIfStatement")
     public static boolean appendSpace(DefaultTokenArtifactData d, DefaultTokenArtifactData next) {
-        if (d.getAction().equals("Delimiter.Operator.Assignment")) {
+        String ca = d.getAction();
+        if (ca.equals("Delimiter.Operator.Assignment")) {
             return true;
 
-        } else if (d.getAction().equals(LilypondReader.PARSER_ACTION_LINEBREAK) ||
-                d.getAction().equals("Delimiter.Separator") ||
-                d.getAction().startsWith("Delimiter.ModeChange.") ||
-                d.getAction().startsWith("Delimiter.Operator.") ||
-                d.getAction().startsWith("Delimiter.Scheme.") ||
-                d.getAction().equals("Comment")) {
+        } else if (ca.equals(LilypondReader.PARSER_ACTION_LINEBREAK) ||
+                ca.equals("Delimiter.Separator") ||
+                ca.startsWith("Delimiter.ModeChange.") ||
+                ca.startsWith("Delimiter.Operator.") ||
+                ca.startsWith("Delimiter.Scheme.") ||
+                ca.equals("Comment")) {
             return false;
         }
 
         if (next != null) {
-            if (d.getAction().equals("Literal.String") && next.getAction().equals("Literal.String")) {
+            String na = next.getAction();
+            if (ca.equals("Literal.String") && na.equals("Literal.String")) {
                 return false;
-            } else if (d.getAction().startsWith("Literal.Number") && next.getAction().startsWith("Literal.Number")) {
-                return false;
-            }
-            if (next.getAction().equals(LilypondReader.PARSER_ACTION_LINEBREAK) ||
-                next.getAction().equals("Delimiter.Separator")) {
+            } else if (ca.startsWith("Literal.Number") && na.startsWith("Literal.Number")) {
                 return false;
             }
-
-            if (next.getAction().startsWith("Pitch.") ||
-                    next.getAction().startsWith("Number.Duration.") ||
-                    next.getAction().startsWith("Name.Builtin.Dynamic") ||
-                    next.getAction().startsWith("Name.Script.Articulation") &&
-                            !d.getAction().startsWith("Literal.") ||
-                    next.getAction().startsWith("Name.Symbol.Spanner") ||
-                    next.getAction().startsWith("Delimiter.Separator") ||
-                    next.getAction().equals("Text.Music.Pitch.Octave") ||
-                    next.getAction().equals("Delimiter.Direction")) {
+            if (na.equals(LilypondReader.PARSER_ACTION_LINEBREAK) ||
+                na.equals("Delimiter.Separator")) {
                 return false;
             }
 
-            if ((d.getAction().startsWith("Text.Music.") ||
-                    d.getAction().startsWith("Literal.Number.Duration")) &&
-                    next.getAction().startsWith("Literal.Number.Duration")) {
+            if (na.startsWith("Pitch.") ||
+                    na.startsWith("Number.Duration.") ||
+                    na.startsWith("Name.Builtin.Dynamic") ||
+                    na.startsWith("Name.Script.Articulation") &&
+                            !ca.startsWith("Literal.") ||
+                    na.startsWith("Name.Symbol.Spanner") ||
+                    na.startsWith("Delimiter.Separator") ||
+                    na.equals("Text.Music.Pitch.Octave") ||
+                    na.equals("Delimiter.Direction")) {
                 return false;
             }
 
-            if ((d.getAction().equals("Delimiter.Direction")) &&
-                    next.getAction().startsWith("Literal.Character.Script")) {
+            if ((ca.startsWith("Text.Music.") ||
+                    ca.startsWith("Literal.Number.Duration")) &&
+                    na.startsWith("Literal.Number.Duration")) {
+                return false;
+            }
+
+            if ((ca.equals("Delimiter.Direction")) &&
+                    na.startsWith("Literal.Character.Script")) {
                 return false;
             }
         }
