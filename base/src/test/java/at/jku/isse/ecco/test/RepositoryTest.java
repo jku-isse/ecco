e2232947ef1f8d4c60a443c43bd14effa2a4c28d
Matthias Preuner <matthias.preuner@gmail.com>
2023-04-17 11:24:28 +0200

    reverting global adapter test, since they are now in the python module

--- base/src/test/java/at/jku/isse/ecco/test/RepositoryTest.java
+++ base/src/test/java/at/jku/isse/ecco/test/RepositoryTest.java
@@ -1,6 +1,4 @@
 package at.jku.isse.ecco.test;
-
-import at.jku.isse.ecco.adapter.ArtifactPlugin;
 import at.jku.isse.ecco.service.EccoService;
 import com.google.common.io.RecursiveDeleteOption;
 import org.testng.Assert;
@@ -9,16 +7,13 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import static com.google.common.io.MoreFiles.deleteDirectoryContents;
-
 public class RepositoryTest {
     Path basePath = Path.of("D:\\Eigene Daten\\Studium\\Studium\\LVAs\\6_Semester\\Bsc\\ecco\\examples\\image_variants"); //TODO adapt path
 
@@ -127,7 +122,7 @@ public class RepositoryTest {
     @Test(groups = {"integration", "gui"})
     public void setupReproForManualInspectionBigHistory() {
 
-        long starttime = System.currentTimeMillis();
+        long starttime =  System.currentTimeMillis();
         // open repository
         EccoService service = new EccoService();
 
@@ -150,11 +145,11 @@ public class RepositoryTest {
         List<Long> time = new ArrayList<>();
         File[] files = basePath.toFile().listFiles();
         long prevTime = System.currentTimeMillis();
-        for (int commitNumber = 1; commitNumber < files.length; commitNumber++) {
+        for(int commitNumber = 1; commitNumber < files.length; commitNumber++) {
             service.setBaseDir(basePath.resolve(files[commitNumber].getName()));
             System.out.println(files[commitNumber].getName());
             service.commit(commitNumber + ". Commit: " + files[commitNumber].getName());
-            time.add(System.currentTimeMillis() - prevTime);
+            time.add(System.currentTimeMillis()- prevTime);
             prevTime = System.currentTimeMillis();
         }
         System.out.println(System.currentTimeMillis() - starttime);
@@ -163,91 +158,4 @@ public class RepositoryTest {
         System.out.println("Nr of Ass." + service.getRepository().getAssociations().size());
     }
 
-    @Test(groups = {"integration", "gui"})
-    public void populatePythonTests() {
-
-        // open repository
-        EccoService service = new EccoService();
-
-        Path eccoPath = Path.of(System.getProperty("user.dir")).getParent();
-        Path repoPath = eccoPath.resolve("examples").resolve("python_variants");
-
-        String repo = ".ecco";
-        Path p = repoPath.resolve(repo);
-        deleteDir(p);
-        Assert.assertFalse(Files.exists(p));
-
-        service.setRepositoryDir(p);
-        service.init();
-
-        Collection<ArtifactPlugin> coll = service.getArtifactPlugins();
-        boolean pythonPluginLoaded = service.getArtifactPlugins().stream().anyMatch(pl -> pl.getName().equals("PythonArtifactPlugin"));
-        if(!pythonPluginLoaded){
-            System.out.println("Python Plugin not loaded ... skipping tests...");
-            return;
-        }
-
-        // make commits
-        String[] commits = new String[]{
-                "V1_purpleshirt",
-                "V2_stripedshirt",
-                "V3_purpleshirt_jacket",
-                "V4_purpleshirt_jacket_glasses",
-                "V5_stripedshirt_jacket_glasses",
-                "V6_stripedshirt_glasses",
-                "V7_purpleshirt_glasses",
-                "V8_stripedshirt_jacket",
-                "V9_stripedshirt_jacket_hat"
-        };
-
-        for (int i = 0; i < commits.length; i++) {
-            service.setBaseDir(repoPath.resolve(commits[i]));
-            service.commit(commits[i]);
-
-            System.out.printf("Commit %d successful\n", i + 1);
-        }
-
-        // checkout variants
-        String[] checkouts = new String[]{
-                "person.1, purpleshirt.1, glasses.1, hat.1",
-                "person.1",
-                "purpleshirt.1",
-                "jacket.1",
-                "stripedshirt.1",
-                "glasses.1",
-                "hat.1"
-        };
-
-        for (int i = 0; i < checkouts.length; i++) {
-            String name = "C" + i + "_" + checkouts[i].replaceAll("[.][0-9]+", "").replaceAll(", ", "_");
-            Path compositionPath = repoPath.resolve(name);
-            deleteDir(compositionPath);
-            createDir(compositionPath);
-            service.setBaseDir(compositionPath);
-            service.checkout(checkouts[i]);
-
-            System.out.printf("Checkout %d successful\n", i + 1);
-        }
-
-        service.close();
-    }
-
-    private void deleteDir(Path p) {
-        try {
-            deleteDirectoryContents(p, RecursiveDeleteOption.ALLOW_INSECURE);       //ALLOW INSECURE
-            Files.delete(p);        //Works only if the dir is already empty. (done by  deleteDirectoryContents)
-        } catch (NoSuchFileException e) {
-            // ignore
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void createDir(Path p) {
-        try {
-            Files.createDirectories(p);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
