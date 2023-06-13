3b0dfedd8f1b2d085a32e469020e1eb8e4aa5a72
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2023-06-13 14:20:30 +0200

    adds a few tests

--- adapter/designspace/src/main/java/at/jku/isse/ecco/adapter/designspace/DesignSpaceReader.java
+++ adapter/designspace/src/main/java/at/jku/isse/ecco/adapter/designspace/DesignSpaceReader.java
@@ -1,16 +1,27 @@
 package at.jku.isse.ecco.adapter.designspace;
 
 import at.jku.isse.designspace.sdk.core.model.Workspace;
+import at.jku.isse.designspace.sdk.core.operations.Operation;
 import at.jku.isse.ecco.adapter.ArtifactReader;
+import at.jku.isse.ecco.adapter.designspace.artifact.OperationArtifact;
 import at.jku.isse.ecco.adapter.designspace.exception.MultipleWorkspaceException;
 import at.jku.isse.ecco.adapter.designspace.exception.NoWorkspaceException;
+import at.jku.isse.ecco.dao.EntityFactory;
 import at.jku.isse.ecco.service.listener.ReadListener;
 import at.jku.isse.ecco.tree.Node;
+import com.google.inject.Inject;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
 public class DesignSpaceReader implements ArtifactReader<Workspace, Set<Node.Op>> {
+    private final EntityFactory entityFactory;
+
+    @Inject
+    public DesignSpaceReader(EntityFactory entityFactory) {
+        this.entityFactory = entityFactory;
+    }
 
     @Override
     public String getPluginId() {
@@ -33,7 +44,25 @@ public class DesignSpaceReader implements ArtifactReader<Workspace, Set<Node.Op>
             throw new NoWorkspaceException();
         }
 
-        return null;
+        Node.Op rootNode = entityFactory.createRootNode();
+        Node.Op currentElementCreationNode = rootNode;
+        Node.Op currentElementDeletionNode = rootNode;
+        Node.Op currentPropertyCreateNode = rootNode;
+        Node.Op currentPropertyDeleteNode = rootNode;
+        Node.Op currentPropertyUpdateAddNode = rootNode;
+        Node.Op currentPropertyUpdateSetNode = rootNode;
+        Node.Op currentPropertyUpdateRemoveNode = rootNode;
+        Collection<Operation> operations = base.operationsProcessed.values();
+
+        for (Operation operation :operations) {
+            OperationArtifact operationArtifact = new OperationArtifact(operation);
+            Node.Op node = entityFactory.createNode(operationArtifact);
+
+            currentNode.addChild(node);
+            currentNode = node;
+        }
+
+        return Set.of(rootNode);
     }
 
     @Override
--- adapter/designspace/src/test/java/at/jku/isse/ecco/adapter/designspace/DesignSpaceReaderTest.java
+++ adapter/designspace/src/test/java/at/jku/isse/ecco/adapter/designspace/DesignSpaceReaderTest.java
@@ -2,43 +2,88 @@ package at.jku.isse.ecco.adapter.designspace;
 
 import at.jku.isse.designspace.sdk.core.model.Workspace;
 import at.jku.isse.designspace.sdk.core.operations.*;
+import at.jku.isse.ecco.adapter.designspace.artifact.OperationArtifact;
 import at.jku.isse.ecco.adapter.designspace.exception.MultipleWorkspaceException;
 import at.jku.isse.ecco.adapter.designspace.exception.NoWorkspaceException;
+import at.jku.isse.ecco.storage.mem.dao.MemEntityFactory;
 import at.jku.isse.ecco.tree.Node;
+import at.jku.isse.ecco.tree.RootNode;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class DesignSpaceReaderTest {
     @Test
     public void multipleWorkspacesAreNotSupported() {
-        DesignSpaceReader reader = new DesignSpaceReader();
+        DesignSpaceReader reader = new DesignSpaceReader(null);
         assertThrows(MultipleWorkspaceException.class, () -> reader.read(new Workspace[2]));
         assertThrows(MultipleWorkspaceException.class, () -> reader.read(mock(Workspace.class), new Workspace[1]));
     }
 
     @Test
     public void noWorkspaceGivenRaisesException() {
-        DesignSpaceReader reader = new DesignSpaceReader();
+        DesignSpaceReader reader = new DesignSpaceReader(null);
         assertThrows(NoWorkspaceException.class, () -> reader.read(null));
         assertThrows(NoWorkspaceException.class, () -> reader.read(null, null));
         assertThrows(NoWorkspaceException.class, () -> reader.read(new Workspace[0]));
     }
 
     @Test
+    public void wrapsElementCreateOperationsInArtefacts() {
+        Operation elementCreateOperation1 = mock(ElementCreate.class);
+        Operation elementCreateOperation2 = mock(ElementCreate.class);
+        Operation elementCreateOperation3 = mock(ElementCreate.class);
+        Operation[] operations = new Operation[]{elementCreateOperation1, elementCreateOperation2, elementCreateOperation3};
+        Workspace workspace = mock(Workspace.class);
+
+        workspace.operationsProcessed = new HashMap<>() {{
+            long id = 1;
+
+            put(id++, elementCreateOperation1);
+            put(id++, elementCreateOperation2);
+            put(id, elementCreateOperation3);
+        }};
+
+        Set<Node.Op> rootNodeOps = new DesignSpaceReader(
+                new MemEntityFactory()
+        ).read(workspace, null);
+        List<Node.Op> rootNodeOpsList = new ArrayList<>(rootNodeOps);
+
+        assertEquals(1, rootNodeOpsList.size());
+        assertInstanceOf(RootNode.class, rootNodeOpsList.get(0));
+
+        List<? extends Node.Op> layer = rootNodeOpsList.get(0).getChildren();
+
+        for (int i = 0; i < 3; i++) {
+            assertNotNull(layer);
+            assertEquals(1, layer.size());
+            assertNotNull(layer.get(0).getArtifact());
+            assertNotNull(layer.get(0).getArtifact().getData());
+            assertInstanceOf(OperationArtifact.class, layer.get(0).getArtifact().getData());
+
+            OperationArtifact operationArtifact = (OperationArtifact) layer.get(0).getArtifact().getData();
+
+            assertNotNull(operationArtifact.getOperation());
+            assertEquals(operations[i], operationArtifact.getOperation());
+
+            layer = layer.get(0).getChildren();
+        }
+    }
+
+    @Test
     public void buildsATreeOfOperations() {
         Operation elementCreateOperation = mock(ElementCreate.class);
-        Operation elementUpdateOperation = mock(ElementUpdate.class);
         Operation elementDeleteOperation = mock(ElementDelete.class);
 
         Operation propertyCreateOperation = mock(PropertyCreate.class);
-        Operation propertyUpdateOperation = mock(PropertyUpdate.class);
         Operation propertyDeleteOperation = mock(PropertyDelete.class);
 
         Operation propertyUpdateAddOperation = mock(PropertyUpdateAdd.class);
@@ -51,11 +96,9 @@ public class DesignSpaceReaderTest {
             long id = 1;
 
             put(id++, elementCreateOperation);
-            put(id++, elementUpdateOperation);
             put(id++, elementDeleteOperation);
 
             put(id++, propertyCreateOperation);
-            put(id++, propertyUpdateOperation);
             put(id++, propertyDeleteOperation);
 
             put(id++, propertyUpdateAddOperation);
@@ -63,31 +106,11 @@ public class DesignSpaceReaderTest {
             put(id, propertyUpdateRemoveOperation);
         }};
 
-        Set<Node.Op> rootNodeOps = new DesignSpaceReader().read(workspace, null);
+        Set<Node.Op> rootNodeOps = new DesignSpaceReader(
+                new MemEntityFactory()
+        ).read(workspace, null);
         List<Node.Op> rootNodeOpsList = new ArrayList<>(rootNodeOps);
 
-        int index = 0;
-
         assertEquals(3, rootNodeOpsList.size());
-
-        assertEquals(elementCreateOperation, rootNodeOpsList.get(index++).getArtifact().getData());
-        assertEquals(elementUpdateOperation, rootNodeOpsList.get(index++).getArtifact().getData());
-        assertEquals(elementDeleteOperation, rootNodeOpsList.get(index++).getArtifact().getData());
-
-        List<? extends Node.Op> elementUpdateNodes = rootNodeOpsList.get(index).getChildren();
-        index = 0;
-
-        assertEquals(3, elementUpdateNodes.size());
-
-        assertEquals(propertyCreateOperation, elementUpdateNodes.get(index++).getArtifact().getData());
-        assertEquals(propertyUpdateOperation, elementUpdateNodes.get(index++).getArtifact().getData());
-        assertEquals(propertyDeleteOperation, elementUpdateNodes.get(index).getArtifact().getData());
-
-        List<? extends Node.Op> propertyUpdateNodes = elementUpdateNodes.get(1).getChildren();
-        index = 0;
-
-        assertEquals(propertyUpdateAddOperation, propertyUpdateNodes.get(index++).getArtifact().getData());
-        assertEquals(propertyUpdateSetOperation, propertyUpdateNodes.get(index++).getArtifact().getData());
-        assertEquals(propertyUpdateRemoveOperation, propertyUpdateNodes.get(index).getArtifact().getData());
     }
 }
