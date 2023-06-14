5cc22bab17e96f760f4bf404e12e49010e6436a2
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2023-06-14 10:20:56 +0200

    actually binds reader and writer to module

--- adapter/designspace/src/main/java/at/jku/isse/ecco/adapter/designspace/DesignSpaceModule.java
+++ adapter/designspace/src/main/java/at/jku/isse/ecco/adapter/designspace/DesignSpaceModule.java
@@ -1,6 +1,37 @@
 package at.jku.isse.ecco.adapter.designspace;
 
+import at.jku.isse.designspace.sdk.core.model.Workspace;
+import at.jku.isse.designspace.sdk.core.operations.Operation;
+import at.jku.isse.ecco.adapter.ArtifactReader;
+import at.jku.isse.ecco.adapter.ArtifactWriter;
+import at.jku.isse.ecco.tree.Node;
 import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
+import com.google.inject.multibindings.Multibinder;
+
+import java.util.HashMap;
+import java.util.Set;
 
 public class DesignSpaceModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        super.configure();
+
+
+        final Multibinder<ArtifactReader<Workspace, Set<Node.Op>>> readerMultibinder =
+                Multibinder.newSetBinder(
+                        binder(),
+                        new TypeLiteral<>() {
+                        });
+
+        readerMultibinder.addBinding().to(WorkspaceReader.class);
+
+        final Multibinder<ArtifactWriter<Set<Node>, HashMap<Long, Operation>>> writerMultibinder =
+                Multibinder.newSetBinder(
+                        binder(),
+                        new TypeLiteral<>() {
+                        });
+
+        writerMultibinder.addBinding().to(OperationsWriter.class);
+    }
 }
