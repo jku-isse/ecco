28d7bf03de1f9135b79c82c96a9de7fdbdced168
Dorkat0 <14137285+Dorkat0@users.noreply.github.com>
2023-10-18 15:41:49 +0200

    checks for "/.dockerenv"
    Fix docker detection on hosts with cgroup v2

--- rest/src/main/java/at/jku/isse/ecco/rest/Settings.java
+++ rest/src/main/java/at/jku/isse/ecco/rest/Settings.java
@@ -36,10 +36,14 @@ public class Settings {
     }
 
     private static Boolean isRunningInsideDocker() {
-        try (Stream<String> stream = Files.lines(Paths.get("/proc/1/cgroup"))) {
-            return stream.anyMatch(line -> line.contains("/docker"));
-        } catch (IOException e) {
-            return false;
+        if (Files.exists(Paths.get("/.dockerenv"))) {
+            return true;
+        } else {
+            try (Stream<String> stream = Files.lines(Paths.get("/proc/1/cgroup"))) {
+                return stream.anyMatch(line -> line.contains("/docker"));
+            } catch (IOException e) {
+                return false;
+            }
         }
     }
 }
