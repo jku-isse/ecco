ed3c7a7613531c25d234582d6699e06860aa08e3
Dorkat0 <14137285+Dorkat0@users.noreply.github.com>
2023-11-17 16:42:20 +0100

    Revert "added a two more test cases for the REST-API."
    This reverts commit 618975ff360e1b925403ebd9cd4b18a3d45a4479.

--- rest/src/test/java/at/jku/isse/ecco/rest/RestTest.java
+++ rest/src/test/java/at/jku/isse/ecco/rest/RestTest.java
@@ -1,17 +1,12 @@
 package at.jku.isse.ecco.rest;
 
-import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
-import io.micronaut.security.authentication.UsernamePasswordCredentials;
-import io.micronaut.security.token.render.BearerAccessRefreshToken;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @MicronautTest
 class RestTest {
@@ -23,37 +18,14 @@ class RestTest {
     @Client("/")
     HttpClient client;
 
-    BearerAccessRefreshToken bearerAccessRefreshToken;
-
     @Test
     void test() {
-        System.out.println("Rest-Test is accessed");
+        System.out.println("TBE");
     }
 
 
     @Test
     void testItWorks() {
-        assertTrue(application.isRunning());
-        System.out.println("Application is running");
-    }
-    @Test
-    @BeforeEach
-    void testAuthenticatedCanFetchUsername() {
-        UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("Tobias", "admin");
-        HttpRequest<?> request = HttpRequest.POST("/login", credentials);
-
-        bearerAccessRefreshToken = client.toBlocking().retrieve(request, BearerAccessRefreshToken.class);
-
-        String repros = client.toBlocking().retrieve(HttpRequest.GET("/api/repository/all")
-                .header("Authorization", "Bearer " + bearerAccessRefreshToken.getAccessToken()), String.class);
-    }
-
-    @Test
-    void checkRepositories() {
-       String repros = client.toBlocking().retrieve(HttpRequest.GET("/api/repository/all")
-                .header("Authorization", "Bearer " + bearerAccessRefreshToken.getAccessToken()), String.class);
-
-        assertTrue(repros.contains("BigHistory_full"));
-        assertTrue(repros.contains("ImageVariants"));
+        Assertions.assertTrue(application.isRunning());
     }
 }
