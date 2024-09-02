3577a05cd251d419e0c2c327b19e5ebe8205ec66
baergump <bernhard.gumpelmair@gmail.com>
2024-09-02 14:13:31 +0200

    Fix Serialization Bug

--- service/src/main/java/at/jku/isse/ecco/storage/mem/pog/MemPartialOrderGraph.java
+++ service/src/main/java/at/jku/isse/ecco/storage/mem/pog/MemPartialOrderGraph.java
@@ -3,6 +3,11 @@ package at.jku.isse.ecco.storage.mem.pog;
 import at.jku.isse.ecco.artifact.Artifact;
 import at.jku.isse.ecco.pog.PartialOrderGraph;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.*;
+
 public class MemPartialOrderGraph implements PartialOrderGraph, PartialOrderGraph.Op {
 
 	public static final long serialVersionUID = 1L;
@@ -10,6 +15,9 @@ public class MemPartialOrderGraph implements PartialOrderGraph, PartialOrderGrap
 	private Node.Op head;
 	private Node.Op tail;
 	private int maxIdentifier = INITIAL_SEQUENCE_NUMBER;
+
+	// used to iteratively serialize in order not to overflow stack
+	private Map<Integer, MemPartialOrderGraphNode> sequenceNumberNodeMap;
 
 	public MemPartialOrderGraph() {
 //		this.head = new MemPartialOrderGraphNode(new MemArtifact<StringArtifactData>(new StringArtifactData("HEAD")));
@@ -19,6 +27,76 @@ public class MemPartialOrderGraph implements PartialOrderGraph, PartialOrderGrap
 		this.head = new MemPartialOrderGraphNode(null);
 		this.tail = new MemPartialOrderGraphNode(null);
 		this.head.addChild(this.tail);
+	}
+
+	private void writeObject(ObjectOutputStream out) throws IOException {
+		this.sequenceNumberNodeMap = new HashMap<>();
+		List<Node.Op> nodes = this.collectNodes();
+		if (nodes.size() > 2){
+			System.out.print("");
+		}
+		for(Node.Op node : nodes){
+			MemPartialOrderGraphNode memPartialOrderGraphNode = (MemPartialOrderGraphNode) node;
+			Artifact<?> artifact = memPartialOrderGraphNode.getArtifact();
+			if (artifact != null) {
+				// head and tail will be serialized as field and must not be in the map
+				Integer sequenceNumber = artifact.getSequenceNumber();
+				if (this.sequenceNumberNodeMap.containsKey(sequenceNumber)) {
+					throw new RuntimeException("Multiple occurences of the same sequence number!");
+				}
+				this.sequenceNumberNodeMap.put(sequenceNumber, memPartialOrderGraphNode);
+			}
+			memPartialOrderGraphNode.prepareSerialization();
+		}
+		out.defaultWriteObject();
+	}
+
+	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+		in.defaultReadObject();
+
+		if (this.head == null) { this.head = new MemPartialOrderGraphNode(null); }
+		if (this.tail == null) { this.tail = new MemPartialOrderGraphNode(null); }
+		((MemPartialOrderGraphNode)this.head).init();
+		((MemPartialOrderGraphNode)this.tail).init();
+
+		// if next of head is empty and previous of tail is empty connect them
+		if (this.sequenceNumberNodeMap == null || this.sequenceNumberNodeMap.size() == 0){
+			this.wireHeadAndTail();
+		}
+
+		// fill collections of nodes using the map
+		for(MemPartialOrderGraphNode node : this.sequenceNumberNodeMap.values()){
+			node.deserializeCollections(this.sequenceNumberNodeMap);
+		}
+
+		MemPartialOrderGraphNode memPartialOrderGraphHead = ((MemPartialOrderGraphNode) this.head);
+		memPartialOrderGraphHead.deserializeCollections(this.sequenceNumberNodeMap);
+		// put head in "previous" of every item in next-collection of head
+		this.head.getNext().forEach(n -> {
+			if (n.getArtifact() != null) {
+				((MemPartialOrderGraphNode) n).addPrevious(this.head);
+			}
+		});
+
+		MemPartialOrderGraphNode memPartialOrderGraphTail = ((MemPartialOrderGraphNode) this.tail);
+		memPartialOrderGraphTail.deserializeCollections(this.sequenceNumberNodeMap);
+		// put tail in "next" of every item in previous-collection of tail
+		this.tail.getPrevious().forEach(n -> {
+			if (n.getArtifact() != null) {
+				((MemPartialOrderGraphNode) n).addNext(this.tail);
+			}
+		});
+
+
+		List<Node.Op> nodes = this.collectNodes();
+		if (nodes.size() > 2){
+			System.out.print("");
+		}
+	}
+
+	private void wireHeadAndTail(){
+		((MemPartialOrderGraphNode) this.head).addNext(this.tail);
+		((MemPartialOrderGraphNode) this.tail).addPrevious(this.head);
 	}
 
 	@Override
--- service/src/main/java/at/jku/isse/ecco/storage/mem/pog/MemPartialOrderGraphNode.java
+++ service/src/main/java/at/jku/isse/ecco/storage/mem/pog/MemPartialOrderGraphNode.java
@@ -6,24 +6,68 @@ import at.jku.isse.ecco.pog.PartialOrderGraph;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Objects;
 
 public class MemPartialOrderGraphNode implements PartialOrderGraph.Node, PartialOrderGraph.Node.Op {
 
 	public static final long serialVersionUID = 1L;
 
-	private Collection<PartialOrderGraph.Node.Op> previous;
-	private Collection<PartialOrderGraph.Node.Op> next;
+	private transient Collection<PartialOrderGraph.Node.Op> previous = new ArrayList<>();
+	private transient Collection<PartialOrderGraph.Node.Op> next = new ArrayList<>();
+
+	// only used for iterative serialization in order not to overflow stack
+	private Collection<Integer> previousSequenceNumbers = new ArrayList<>();
+	// only used for iterative serialization in order not to overflow stack
+	private Collection<Integer> nextSequenceNumbers = new ArrayList<>();
 
 	private Artifact.Op<?> artifact;
 
 	public MemPartialOrderGraphNode(Artifact.Op<?> artifact) {
 //		Objects.requireNonNull(artifact);
 		this.artifact = artifact;
-		this.previous = new ArrayList<>();
-		this.next = new ArrayList<>();
 	}
 
+	public void init(){
+		if (this.next == null){ this.next = new ArrayList<>(); }
+		if (this.previous == null){ this.previous = new ArrayList<>(); }
+	}
+
+	public void prepareSerialization(){
+		this.nextSequenceNumbers = new ArrayList<>();
+		this.previousSequenceNumbers = new ArrayList<>();
+		// fill integer collections, that will be serialized
+		this.previous.forEach(n -> {
+			Artifact<?> artifact = n.getArtifact();
+			if (artifact != null){
+				// head and tail will be put into deserialized node in separate step
+				this.previousSequenceNumbers.add(artifact.getSequenceNumber());
+			}
+		});
+
+		this.next.forEach(n -> {
+			Artifact<?> artifact = n.getArtifact();
+			if (artifact != null){
+				// head and tail will be put into deserialized node in separate step
+				this.nextSequenceNumbers.add(artifact.getSequenceNumber());
+			}
+		});
+	}
+
+	public void deserializeCollections(Map<Integer, MemPartialOrderGraphNode> sequenceNumberNodeMap){
+		if (this.next == null) { this.next = new ArrayList<>(); }
+		if (this.previous == null) { this.previous = new ArrayList<>(); }
+		this.nextSequenceNumbers.forEach(i -> this.next.add(sequenceNumberNodeMap.get(i)));
+		this.previousSequenceNumbers.forEach(i -> this.previous.add(sequenceNumberNodeMap.get(i)));
+	}
+
+	public void addPrevious(PartialOrderGraph.Node.Op node){
+		this.previous.add(node);
+	}
+
+	public void addNext(PartialOrderGraph.Node.Op node){
+		this.next.add(node);
+	}
 	@Override
 	public Collection<Op> getPrevious() {
 		return this.previous;
@@ -65,6 +109,11 @@ public class MemPartialOrderGraphNode implements PartialOrderGraph.Node, Partial
 	@Override
 	public String toString() {
 		return this.getArtifact() == null ? "NULL" : this.getArtifact().toString() + " [" + this.getArtifact().getSequenceNumber() + "]";
+	}
+
+	@Override
+	public int hashCode(){
+		return Objects.hash(this.artifact);
 	}
 
 }
