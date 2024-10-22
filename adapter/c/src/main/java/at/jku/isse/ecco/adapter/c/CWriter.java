ca377ca7f8d4b74a06043a45736218c6fae72a67
baergump <bernhard.gumpelmair@gmail.com>
2024-10-22 15:23:28 +0200

    Implemented Writer for C-Adapter.

--- adapter/c/src/main/java/at/jku/isse/ecco/adapter/c/CWriter.java
+++ adapter/c/src/main/java/at/jku/isse/ecco/adapter/c/CWriter.java
@@ -1,12 +1,22 @@
 package at.jku.isse.ecco.adapter.c;
 
+import at.jku.isse.ecco.EccoException;
 import at.jku.isse.ecco.adapter.ArtifactWriter;
+import at.jku.isse.ecco.adapter.c.data.FunctionArtifactData;
+import at.jku.isse.ecco.adapter.c.data.LineArtifactData;
+import at.jku.isse.ecco.adapter.dispatch.PluginArtifactData;
+import at.jku.isse.ecco.artifact.Artifact;
+import at.jku.isse.ecco.artifact.ArtifactData;
 import at.jku.isse.ecco.service.listener.WriteListener;
 import at.jku.isse.ecco.tree.Node;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 public class CWriter implements ArtifactWriter<Set<Node>, Path> {
@@ -20,8 +30,54 @@ public class CWriter implements ArtifactWriter<Set<Node>, Path> {
 
     @Override
     public Path[] write(Path base, Set<Node> input) {
-        // TODO
-        return new Path[0];
+        List<Path> output = new ArrayList<>();
+
+        for (Node fileNode : input) {
+            Artifact<?> fileArtifact = fileNode.getArtifact();
+            ArtifactData artifactData = fileArtifact.getData();
+            if (!(artifactData instanceof PluginArtifactData pluginArtifactData))
+                throw new EccoException("Expected plugin artifact data.");
+            Path outputPath = base.resolve(pluginArtifactData.getPath());
+            output.add(outputPath);
+
+            List<?> fileNodeChildren = fileNode.getChildren();
+            if (fileNodeChildren.size() != 1){
+                throw new EccoException("Expected ordered node beneath plugin node.");
+            }
+            Node orderedNode = (Node) fileNodeChildren.iterator().next();
+            this.writeCFile(outputPath, orderedNode);
+        }
+
+        return output.toArray(new Path[0]);
+    }
+
+    private void writeCFile(Path filePath, Node orderedNode){
+        try (BufferedWriter bw = Files.newBufferedWriter(filePath)) {
+            List<Node> fileNodeChildren = (List<Node>) orderedNode.getChildren();
+            for (Node node : fileNodeChildren){
+                Artifact<?> artifact = node.getArtifact();
+                ArtifactData artifactData = artifact.getData();
+                if (artifactData instanceof FunctionArtifactData){
+                    this.writeFunctionNode(bw, node);
+                } else if (artifactData instanceof LineArtifactData lineArtifactData){
+                    bw.write(lineArtifactData.getLine());
+                    bw.newLine();
+                } else {
+                    throw new EccoException("Expected FunctionArtifactData or LineArtifactData.");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeFunctionNode(BufferedWriter bw, Node functionNode) throws IOException {
+        List<Node> lineNodeChildren = (List<Node>) functionNode.getChildren();
+        for (Node lineNode : lineNodeChildren){
+            LineArtifactData lineArtifactData = (LineArtifactData) lineNode.getArtifact().getData();
+            bw.write(lineArtifactData.getLine());
+            bw.newLine();
+        }
     }
 
     @Override
