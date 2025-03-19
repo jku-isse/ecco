69251edeaab4e2d34a4de6fed5054fe86b799c53
baergump <bernhard.gumpelmair@pm.me>
2025-03-19 13:39:23 +0100

    feature: add depth-first traversal of ecco trees using visitor-pattern

--- base/src/main/java/at/jku/isse/ecco/tree/Node.java
+++ base/src/main/java/at/jku/isse/ecco/tree/Node.java
@@ -33,6 +33,19 @@ public interface Node extends Persistable {
 	@Override
 	String toString();
 
+	/**
+	 * Traverse the subtree with this node as its root in a depth first manner.
+	 * @param visitor
+	 */
+	default void dfTraverse(NodeVisitor visitor){
+		List<? extends Node> children = this.getChildren();
+		if (children != null) {
+			for (Node child : children) {
+				child.traverse(visitor);
+			}
+		}
+		visitor.visit(this);
+	}
 
 	default void traverse(NodeVisitor visitor) {
 		visitor.visit(this);
